d1df6540f461573fdf2ad1a3a4b4ed1636e5a0e3
Nileema Shah <nileemashah@yahoo.com>
2022-10-12 22:24:06 -0400

    Update server.js

--- config/server.js
+++ config/server.js
@@ -1,7 +1,10 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
-  },
+   app: {
+   keys: [ 'hoZg7SWPAzROtlm4h0LQaA==',
+           '6xaJFUt3M8vLUrUjSd49Pg==',
+           'tXIcX65oY7jhnazXuodGkg==',
+           'fqH/QhhYnkACpWnhnlBsuA==' ]
+ },
 });
